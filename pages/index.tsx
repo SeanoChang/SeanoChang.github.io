5c769371195e934d68934f1a7d54d01998617fd7
SeanoChang <89418312+SeanoChang@users.noreply.github.com>
2022-12-05 00:01:29 -0500

    initial-commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Seano Chang</title>
+        <meta name="description" content="Sean Chang Personal Website" />
+      </Head>
+      <main className={styles.main}>
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
